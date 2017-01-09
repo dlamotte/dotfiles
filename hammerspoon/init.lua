9bc00c9283c805bf27d792a62008db1a7819e810
Dan LaMotte <lamotte85@gmail.com>
2017-01-09 11:17:41 -0600

    hammerspoon: push to talk

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,35 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+local mash = {"ctrl", "alt", "cmd"}
+
+hs.hotkey.bind(mash, "W", function()
     hs.alert.show("hello world")
+end)
+
+hs.hotkey.bind(mash, "M", function()
+    device = hs.audiodevice.findDeviceByName('Built-in Microphone')
+    is_muted = device:muted()
+
+    if is_muted then
+        device:setMuted(not is_muted)
+        hs.alert.show(' mic unmuted')
+    end
+end,
+function()
+    device = hs.audiodevice.findDeviceByName('Built-in Microphone')
+    is_muted = device:muted()
+
+    if not is_muted then
+        device:setMuted(not is_muted)
+        hs.alert.show(' mic muted')
+    end
+end
+)
+
+hs.hotkey.bind(mash, "U", function()
+    device = hs.audiodevice.findDeviceByName('Built-in Microphone')
+    is_muted = device:muted()
+
+    if is_muted then
+        device:setMuted(not is_muted)
+        hs.alert.show(' mic unmuted')
+    end
 end)
