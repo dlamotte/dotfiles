f4061e4183165eaa6c50d0b6ec5a55eddee97daa
Dan LaMotte <lamotte85@gmail.com>
2017-01-13 01:06:51 -0600

    hs/layout: initial working

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,6 +4,90 @@ hs.hotkey.bind({'ctrl', 'alt'}, 'R',
     end
 )
 
+layout = require('layout').new(
+    {
+        {mod = {'ctrl', 'cmd'}, key = 'F', action = 'full'},
+        {mod = {'ctrl', 'cmd'}, key = 'L', action = 'push-right'},
+        {mod = {'ctrl', 'cmd'}, key = 'H', action = 'push-left'},
+        {mod = {'ctrl', 'alt'}, key = '1', layout = 'laptop'},
+        {mod = {'ctrl', 'alt'}, key = '2', layout = 'thunderbolt'},
+        {mod = {'ctrl', 'alt'}, key = '3', layout = 'thunderbolt2'}
+    },
+    {
+        {
+            app = 'Google Chrome',
+            layouts = {
+                laptop       = {1, 'full'},
+                thunderbolt  = {1, 'half-left'},
+                thunderbolt2 = {1, 'half-right'},
+            }
+        },
+        {
+            app = 'iTerm2',
+            layouts = {
+                laptop       = {1, 'full'},
+                thunderbolt  = {1, 'half-right'},
+                thunderbolt2 = {2, 'half-left'},
+            }
+        },
+        {
+            app = 'Microsoft Outlook',
+            layouts = {
+                laptop       = {1, 'full'},
+                thunderbolt  = {1, 'half-left'},
+                thunderbolt2 = {1, 'half-left'},
+            },
+            windows = {
+                {re = '[0-9]+ Reminders', layouts = {
+                    laptop       = {1, 'fullish-bottom'},
+                    thunderbolt  = {1, 'halfish-right-bottom'},
+                    thunderbolt2 = {1, 'halfish-right-bottom'},
+                }}
+            }
+        },
+        {
+            app = 'Slack',
+            layouts = {
+                laptop       = {1, 'full'},
+                thunderbolt  = {1, 'half-left'},
+                thunderbolt2 = {1, 'half-left'},
+            }
+        },
+        {
+            app = 'Gitter',
+            layouts = {
+                laptop       = {1, 'full'},
+                thunderbolt  = {1, 'half-left'},
+                thunderbolt2 = {1, 'half-left'},
+            }
+        },
+        {
+            app = 'Sococo',
+            layouts = {
+                laptop       = {1, 'full'},
+                thunderbolt  = {1, 'half-right'},
+                thunderbolt2 = {2, 'half-right'},
+            }
+        },
+        {
+            app = 'Jabber',
+            layouts = {
+                laptop       = {1, 'jabber'},
+                thunderbolt  = {1, 'jabber'},
+                thunderbolt2 = {2, 'jabber'},
+            },
+            windows = {
+                {noteq = 'Cisco Jabber', layouts = {
+                    laptop       = {1, 'jabber-convo'},
+                    thunderbolt  = {1, 'jabber-convo-half-right'},
+                    thunderbolt2 = {2, 'jabber-convo-half-right'},
+                }}
+            }
+        },
+    }
+)
+layout:start()
+
 push2talk = require('push2talk').new({'ctrl', 'alt'}, 'M', 'U')
 push2talk:start()
 
---
+++ hammerspoon/layout.lua
@@ -0,0 +1,179 @@
+local Layout = {}
+Layout.__index = Layout
+
+Layout.res_1440_900 = hs.geometry.rect(0.0,0.0,1440.0,900.0)   -- laptop
+Layout.res_1680_1050 = hs.geometry.rect(0.0,0.0,1680.0,1050.0) -- thunderbolt
+
+function Layout.new(hotkeys, layouts)
+    local self = setmetatable({}, Layout)
+    self.hotkeys = hotkeys
+    self.layout = nil
+    self.layouts = layouts
+    return self
+end
+
+function Layout:action(name)
+    local win = hs.window.focusedWindow()
+    local screen = win:screen()
+    local frame = screen:frame()
+    local update = false
+
+    if name == 'full' then
+        update = true
+
+    elseif name == 'push-right' then
+        frame.w = frame.w / 2
+        frame.x1 = frame.x1 + frame.w
+        update = true
+
+    elseif name == 'push-left' then
+        frame.w = frame.w / 2
+        update = true
+
+    end
+
+    if update then
+        win:setFrame(frame, 0)
+    end
+end
+
+function Layout:bind()
+    for _, d in ipairs(self.hotkeys) do
+        if d.action then
+            hs.hotkey.bind(d.mod, d.key, function()
+                self:action(d.action)
+            end)
+        elseif d.layout then
+            hs.hotkey.bind(d.mod, d.key, function()
+                self.layout = d.layout
+                self:render()
+            end)
+        end
+    end
+end
+
+function Layout:detect()
+    local screens = hs.screen.allScreens()
+
+    if #screens == 1 then
+        if screens[1]:fullFrame() == self.res_1440_900 then
+            self.layout = 'laptop'
+
+        elseif screens[1]:fullFrame() == self.res_1680_1050 then
+            self.layout = 'thunderbolt'
+
+        else
+            self.layout = 'laptop'
+        end
+    else
+        -- currently, only other case I care to handle
+        self.layout = 'thunderbolt2'
+    end
+end
+
+function Layout:render(onlyapp)
+    local screens = hs.screen.allScreens()
+    for _, def in ipairs(self.layouts) do
+        local app = hs.application.get(def.app)
+        local layout = def.layouts[self.layout]
+
+        if onlyapp and app ~= onlyapp then
+            app = nil
+        end
+
+        if app then
+            local screen = screens[layout[1]]
+            local position = layout[2]
+
+            for _, win in ipairs(app:allWindows()) do
+                if def.windows then
+                    local title = win:title()
+                    for _, defwin in ipairs(def.windows) do
+                        if (defwin.re and string.match(title, defwin.re))
+                            or (defwin.noteq and title ~= defwin.noteq) then
+
+                            screen = screens[defwin.layouts[self.layout][1]]
+                            position = defwin.layouts[self.layout][2]
+                            break
+                        end
+                    end
+                end
+
+                if not screen then
+                    screen = screens[1]
+                end
+
+                win:moveToScreen(screen, 0)
+                local frame = screen:frame()
+                local new = screen:frame()
+                local update = false
+
+                if position == 'full' then
+                    update = true
+
+                elseif position == 'half-left' then
+                    new.w = new.w / 2
+                    update = true
+
+                elseif position == 'half-right' then
+                    new.w = new.w / 2
+                    new.x1 = new.x1 + new.w
+                    update = true
+
+                elseif position == 'fullish-bottom' then
+                    new.x1 = new.x1 + 100
+                    new.x2 = frame.x2 - 200
+                    update = true
+
+                elseif position == 'halfish-right-bottom' then
+                    new.x1 = new.x1 + 100
+                    new.x2 = frame.x2 / 2 - 200
+                    new.y1 = new.y1 + new.y2 - 200
+                    update = true
+
+                elseif position == 'jabber' then
+                    new.x1 = frame.x2 - 450
+                    new.w = 450
+                    update = true
+
+                elseif position == 'jabber-convo' then
+                    new.x2 = frame.x2 - 455
+                    update = true
+
+                elseif position == 'jabber-convo-half-right' then
+                    new.x1 = frame.x2 / 2
+                    new.x2 = frame.x2 - 455
+                    update = true
+
+                end
+
+                if update then
+                    win:setFrame(new, 0)
+                end
+            end
+        end
+    end
+end
+
+function Layout:start()
+    self:bind()
+    self:detect()
+    self:render()
+
+    self.screen_watcher = hs.screen.watcher.new(function()
+        self:detect()
+        self:render()
+    end)
+    self.screen_watcher:start()
+
+    self.app_watcher = hs.application.watcher.new(
+        function(appname, evtype, app)
+            if app and evtype == hs.application.watcher.launched then
+                self:render(app)
+            end
+        end
+    )
+    self.app_watcher:start()
+end
+
+return Layout
