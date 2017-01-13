9d5a4e93d1639f3beb60a225f558862364d83914
Dan LaMotte <lamotte85@gmail.com>
2017-01-12 18:16:16 -0600

    hammerspoon: mic icon instead of ugly text

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,8 +1,69 @@
 local mash = {"ctrl", "alt", "cmd"}
-
 local mb_mic = hs.menubar.new()
 mb_mic:priority(0)
-mb_mic:setTitle('mic unmuted')
+
+local icon_muted = hs.image.imageFromASCII(table.concat({
+    '................',
+    '..S..F....F.....',
+    '.R..............',
+    '................',
+    '................',
+    '................',
+    '.....A....B.....',
+    '................',
+    '.....H....H.....',
+    '.....F....F.....',
+    '..1..D....C..8..',
+    '..2..........7..',
+    '.......HH.......',
+    '...3........6...',
+    '......J..K....P.',
+    '.......45....Q..',
+    '................',
+    '.......ML.......'}, '\n'),
+    {{
+        strokeColor = {alpha = 1},
+        fillColor   = {alpha = 0},
+        strokeWidth = 2,
+        shouldClose = false,
+--        antialias = false
+    },
+    {
+        shouldClose = true
+    }}
+)
+local icon_unmuted = hs.image.imageFromASCII(table.concat({
+    '................',
+    '.....F....F.....',
+    '................',
+    '................',
+    '................',
+    '................',
+    '.....A....B.....',
+    '................',
+    '.....H....H.....',
+    '.....F....F.....',
+    '..1..D....C..8..',
+    '..2..........7..',
+    '.......HH.......',
+    '...3........6...',
+    '......J..K......',
+    '.......45.......',
+    '................',
+    '.......ML.......'}, '\n'),
+    {{
+        strokeColor = {alpha = 1},
+        fillColor   = {alpha = 0},
+        strokeWidth = 2,
+        shouldClose = false,
+--        antialias = false
+    },
+    {
+        shouldClose = true
+    }}
+)
+
+mb_mic:setIcon(icon_unmuted)
 
 hs.hotkey.bind(mash, "W", function()
     hs.alert.show("hello world")
@@ -21,7 +82,7 @@ hs.hotkey.bind({'ctrl', 'alt'}, "M", function()
 
     if is_changed then
         hs.alert.show(' mic unmuted')
-        mb_mic:setTitle('mic unmuted')
+        mb_mic:setIcon(icon_unmuted)
     end
 end,
 function()
@@ -37,7 +98,7 @@ function()
 
     if is_changed then
         hs.alert.show(' mic muted')
-        mb_mic:setTitle('mic muted')
+        mb_mic:setIcon(icon_muted)
     end
 end
 )
@@ -55,6 +116,6 @@ hs.hotkey.bind({'ctrl', 'alt'}, "U", function()
 
     if is_changed then
         hs.alert.show(' mic unmuted')
-        mb_mic:setTitle('mic unmuted')
+        mb_mic:setIcon(icon_unmuted)
     end
 end)
