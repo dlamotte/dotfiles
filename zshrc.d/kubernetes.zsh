d50db6813ed9b668a86654cf8d67a1752e81bab5
Dan LaMotte <lamotte85@gmail.com>
2018-04-20 14:41:02 -0500

    zsh: retire kube-namespace

--- zshrc.d/kubernetes.zsh
+++ zshrc.d/kubernetes.zsh
@@ -1,12 +1,2 @@
-kube-namespace() {
-    if [[ -z "$1" ]]; then
-        unset KUBE_NAMESPACE
-        unalias kubectl
-    else
-        export KUBE_NAMESPACE="$1"
-        alias kubectl="kubectl --namespace ""$KUBE_NAMESPACE"""
-    fi
-}
-
 source <(kubectl completion zsh)
 source <(helm completion zsh)
