6b82bd184f10723a4783971de864d2a9981da84a
Dan LaMotte <lamotte85@gmail.com>
2023-09-20 14:36:50 -0500

    zshrc: reinit goenv env when go version changes
    I don't understand why this isn't in the install instructions, but
    vim-go is completely broken without this (and anything that depends on
    the environment to not lie to them).
    
    https://github.com/go-nv/goenv/blob/master/INSTALL.md

--- zshrc.d/goenv.zsh
+++ zshrc.d/goenv.zsh
@@ -1,1 +1,10 @@
 eval "$(goenv init -)"
+
+_reinit_goenv_version=$(go version)
+_reinit_goenv_when_changed() {
+    local v=$(go version)
+    if [[ $v != $_reinit_goenv_version ]]; then
+        eval "$(goenv init -)"
+    fi
+}
+precmd_functions+=(_reinit_goenv_when_changed)
