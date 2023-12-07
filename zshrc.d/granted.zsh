11af808d7dc62f1f87f0887e3f9a16582a4860b4
Dan LaMotte <lamotte85@gmail.com>
2023-12-07 13:16:26 -0600

    zshrc.d/granted: unset creds in environment; prefer AWS_PROFILE

--- zshrc.d/granted.zsh
+++ zshrc.d/granted.zsh
@@ -2,4 +2,11 @@
 # brew install granted
 # run: assume
 
-alias assume="source assume"
+function assume() {
+    source assume "$@"
+
+    # granted sets these as well as AWS_PROFILE, these are problematic for any
+    # terraform that points to a specific profile as aws sdks prefer env creds
+    # to profile settings by default
+    unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
+}
